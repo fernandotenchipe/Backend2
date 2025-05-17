1bdfe65be0d5cf70949fde78db04ac54deb276cc
Fernando Tenchipe Morales <a01571277@tec.mx>
2025-05-17 00:12:28 -0600

    fix

--- server/utils/mongodb.js
+++ server/utils/mongodb.js
@@ -1,8 +1,6 @@
 import mongoose from 'mongoose';	
 
 export const connectDB = async () => {
-    console.log("🔍 ENV keys:", Object.keys(process.env));
-console.log("🧪 MONGO_URL:", process.env.MONGO_URL);
     await mongoose.connect(process.env.MONGO_URL)
     console.log('MongoDB connected')
 }
