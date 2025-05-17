3e00800265ae739a71a36f3aace3f8001c8c885c
Fernando Tenchipe Morales <a01571277@tec.mx>
2025-05-17 00:01:24 -0600

    fix

--- server/utils/mongodb.js
+++ server/utils/mongodb.js
@@ -1,6 +1,8 @@
 import mongoose from 'mongoose';	
 
 export const connectDB = async () => {
+    console.log("🔍 ENV keys:", Object.keys(process.env));
+console.log("🧪 MONGO_URL:", process.env.MONGO_URL);
     await mongoose.connect(process.env.MONGO_URL)
     console.log('MongoDB connected')
 }
