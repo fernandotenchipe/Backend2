9f837b906bdfc518ad4a9a7d8fc572eb5aaf79f6
Fernando Tenchipe Morales <a01571277@tec.mx>
2025-05-18 10:26:00 -0600

    fix

--- server/routes/items.routes.js
+++ server/routes/items.routes.js
@@ -5,11 +5,7 @@ import { validateJWT } from "../utils/jwt.js";
 
 const router = Router();
 
-router.get("/items/", validateJWT, (req, res) => {
-  console.log("🚚 ENTRÓ A GET /items");
-  console.log("🧪 TOKEN:", req.headers.authorization);
-  res.json({ message: "RUTA FUNCIONANDO" });
-});
+router.get('/items/',validateJWT, getItems);
 router.get('/items/:id',validateJWT, getItem);
 router.post('/items/',validateJWT, postItem);
 router.put('/items/:id',validateJWT, putItem);
