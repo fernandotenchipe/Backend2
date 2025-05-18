3863f9838b046f4f30b4dc1a555bdd711a4c6e4c
Fernando Tenchipe Morales <a01571277@tec.mx>
2025-05-18 02:24:46 -0600

    fix

--- server/routes/items.routes.js
+++ server/routes/items.routes.js
@@ -5,7 +5,7 @@ import { validateJWT } from "../utils/jwt.js";
 
 const router = Router();
 
-router.get('/items/',validateJWT, getItems);
+router.get('/items',validateJWT, getItems);
 router.get('/items/:id',validateJWT, getItem);
 router.post('/items/',validateJWT, postItem);
 router.put('/items/:id',validateJWT, putItem);
