db27784ba863bf68394e37cf23641cf6d5be7913
Fernando Tenchipe Morales <a01571277@tec.mx>
2025-06-01 20:35:02 -0600

    practice

--- server/utils/jwt.js
+++ server/utils/jwt.js
@@ -1,27 +1,26 @@
 import jwt from "jsonwebtoken";
-import {Router} from "express"; 
 
-export const validateJWT = Router();
+export const validateJWT = (req, res, next) => {
+  console.log("🧾 HEADERS EN BACKEND (GET):", req.headers);
 
-validateJWT.use((req, res, next) => {
-    console.log("🧾 HEADERS EN BACKEND (GET):", req.headers);
+  let token = req.headers.authorization;
+  if (!token) {
+    return res.status(401).json({ message: "No token provided" });
+  }
 
-    let token = req.headers.authorization;
-    if (!token) {
-        return res.status(401).json({ message: "No token provided" });
+  if (token.startsWith("Bearer ")) {
+    token = token.split(" ")[1];
+  }
+
+  console.log("🧪 TOKEN recibido:", token);
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) {
+      console.log("❌ Token inválido:", err.message);
+      return res.status(401).json({ message: "Invalid token: " + err.message });
     }
-    if(token.startsWith("Bearer ")){
-        token =token.split(" ")[1];
-    }
-    console.log("🧪 TOKEN crudo recibido:", req.headers.authorization);
-    jwt.verify(token, process.env.JWT_SECRET, (e, decoded) => {
-        if (e) {
-             console.log("❌ Token inválido:", e.message);
-            res.status(401).json({ message: "Invalid token" + e.message });
-        }
-        else {
-            req.user = decoded;
-            next();
-        }
-    });
-})
+
+    req.user = decoded;
+    next();
+  });
+};
